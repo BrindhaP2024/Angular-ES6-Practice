7e04b6d4d918dd794a68de573a3e8a34700dc520
BrindhaP2024 <brindha.palani@aspiresys.com>
2025-01-27 09:21:39 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,RouterLinkActive, RouterLink} from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ComponentServiceComponent } from "./components/component-service/component-service.component";
 import { CardComponent } from "./components/card/card.component";
+import { ParentComponent } from "./components/parent/parent.component";
 
 
 @Component({

f84c37874be91ae44ee1b5a51291ee1bc8a13707
BrindhaP2024 <brindha.palani@aspiresys.com>
2025-01-30 17:52:02 +0530

    PIpes basic example

--- src/app/components/signal-example/signal-example.component.ts
+++ src/app/components/signal-example/signal-example.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal-example',
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class SignalExampleComponent {
 
+  count = signal(0);
+
+  increment(): void {
+    this.count.set(this.count() + 1);
+  }
 }
+
+
+
